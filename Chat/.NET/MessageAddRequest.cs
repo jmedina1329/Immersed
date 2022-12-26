17a20b9904f2a2ab45366791196e69df31661cf6
Joe Medina <112432330+jmedina1329@users.noreply.github.com>
2022-12-26 13:20:32 -0500

    Update MessageAddRequest.cs

--- Chat/.NET/MessageAddRequest.cs
+++ Chat/.NET/MessageAddRequest.cs
@@ -1,21 +1,21 @@
 public class MessageAddRequest
-    {
-        [Required,MinLength(2), MaxLength(1000)]
-        public string Message { get; set; }
-        [Required,MinLength(2), MaxLength(100)]
-        public string Subject { get; set; }
-        [Required,Range(1,Int32.MaxValue)]
-        public int RecipientEntityTypeId { get; set; }
-        [Required, Range(1,Int32.MaxValue)]
-        public int RecipientId { get; set; }
-        [Required,Range(1,Int32.MaxValue)]
-        public int SenderEntityTypeId { get; set; }
-        [Required,Range(1,Int32.MaxValue)]
-        public int SenderId { get; set; }
-        [Required,Range(1,Int32.MaxValue)]
-        public int ZoneId { get; set; }
-        [AllowNull]
-        public DateTime? DateSent { get; set; }
-        [AllowNull]
-        public DateTime? DateRead { get; set; }
-    }
+{
+    [Required,MinLength(2), MaxLength(1000)]
+    public string Message { get; set; }
+    [Required,MinLength(2), MaxLength(100)]
+    public string Subject { get; set; }
+    [Required,Range(1,Int32.MaxValue)]
+    public int RecipientEntityTypeId { get; set; }
+    [Required, Range(1,Int32.MaxValue)]
+    public int RecipientId { get; set; }
+    [Required,Range(1,Int32.MaxValue)]
+    public int SenderEntityTypeId { get; set; }
+    [Required,Range(1,Int32.MaxValue)]
+    public int SenderId { get; set; }
+    [Required,Range(1,Int32.MaxValue)]
+    public int ZoneId { get; set; }
+    [AllowNull]
+    public DateTime? DateSent { get; set; }
+    [AllowNull]
+    public DateTime? DateRead { get; set; }
+}
