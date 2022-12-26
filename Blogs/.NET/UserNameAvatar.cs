6e4e3b073aa71f4efb19c398c48358ea58eefda9
Joe Medina <112432330+jmedina1329@users.noreply.github.com>
2022-12-26 14:45:09 -0500

    Update and rename Blogs/UserNameAvatar.cs to Blogs/.NET/UserNameAvatar.cs

---
+++ Blogs/.NET/UserNameAvatar.cs
@@ -0,0 +1,7 @@
+public class UserNameAvatar
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Mi { get; set; }
+    public string AvatarUrl { get; set; }
+}
