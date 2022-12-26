968c86e4a622f611798d4bb420c25ca74afb598f
Joe Medina <112432330+jmedina1329@users.noreply.github.com>
2022-12-26 13:18:15 -0500

    Update and rename Chat/MessageAddRequest.cs to Chat/.NET/MessageAddRequest.cs

--- Chat/.NET/MessageAddRequest.cs
+++ Chat/.NET/MessageAddRequest.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Models.Requests.Messages
-{
-    public class MessageAddRequest
+public class MessageAddRequest
     {
         [Required,MinLength(2), MaxLength(1000)]
         public string Message { get; set; }
@@ -29,4 +19,3 @@ namespace Sabio.Models.Requests.Messages
         [AllowNull]
         public DateTime? DateRead { get; set; }
     }
-}
