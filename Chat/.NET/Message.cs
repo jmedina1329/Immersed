0ef0741ec97e1f39d6190d8848cb3162e007042f
Joe Medina <112432330+jmedina1329@users.noreply.github.com>
2022-12-26 13:20:48 -0500

    Update Message.cs

--- Chat/.NET/Message.cs
+++ Chat/.NET/Message.cs
@@ -1,12 +1,12 @@
 public class Message
-    {
-        public int Id { get; set; }
-        public string MessageBody { get; set; }
-        public string Subject { get; set; }
-        public EntityData RecipientData { get; set; }
-        public EntityData SenderData { get; set; }
-        public int ZoneId { get; set; }
-        public bool IsDeleted { get; set; }
-        public DateTime DateSent { get; set; }
-        public DateTime DateRead { get; set; }
-    }
+{
+    public int Id { get; set; }
+    public string MessageBody { get; set; }
+    public string Subject { get; set; }
+    public EntityData RecipientData { get; set; }
+    public EntityData SenderData { get; set; }
+    public int ZoneId { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime DateSent { get; set; }
+    public DateTime DateRead { get; set; }
+}
